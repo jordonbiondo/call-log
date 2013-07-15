0f24dd124762ea153ad6701177b6453f073ea2cf
unknown <jorbi@HELIUM.configura.local>
2013-07-15 10:55:14 -0400

    safe tracing, buffer and time customization, writing and coloring fixes

--- call-log.el
+++ call-log.el
@@ -45,72 +45,122 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 
 ;;; Code:
-(defun clog/msg(msg &optional out-buffer)
-  (clog/write (format "%s %s"
-		     (clog/-color-text "clog/MSG:" 'font-lock-constant-face)
-		     (clog/make-string msg)) out-buffer))
 
-(defun clog/bug(msg &optional out-buffer)
-  (clog/write (format "%s %s"
-		     (clog/-color-text "clog/BUG:" 'font-lock-warning-face)
-		     (clog/make-string msg)) out-buffer))
+;;---------------------------------------------------------------------------
+;; Output buffer
+;;---------------------------------------------------------------------------  
+(defvar clog/output-buffer "*Messages*"
+  "The name of the buffer where all clog messages will be logged.")
 
-(defun clog/todo(msg &optional out-buffer)
-  (clog/write (format "%s %s"
-		     (clog/-color-text "clog/TODO:" 'font-lock-variable-name-face)
-		     (clog/make-string msg)) out-buffer))
+(defun clog/-get-output-buffer()
+  "Internal function used to return the buffer specified by `clog/output-buffer', if it does not exist, it will be created .If `clog/output-buffer' is nil, returns the *Messages* buffer."
+  (let ((buf (get-buffer clog/output-buffer)))
+    (if (and buf (bufferp buf)) buf
+      (if clog/output-buffer
+	  (generate-new-buffer (generate-new-buffer-name clog/output-buffer))
+	(get-buffer "*Messages*")))))
 
+;;---------------------------------------------------------------------------
+;; Time stamp
+;;---------------------------------------------------------------------------  
+(defvar clog/time-format "%Y-%m-%dT%T%z"
+  "Format of the time stamp on each log message. See `format-time-string' for details.")
 
-(defun clog/up-to-func()
+(defun clog/-time-string()
+  "Internal function used to return a the formated time string specifed by `clog/time-format'.
+If `clog/time-format is nil, return the default ISO 8601 format"
+  (let (time-str (format-time-string clog/time-format))
+    (if time-str time-str (format-time-string "%Y-%m-%dT%T%z"))))
+
+;;---------------------------------------------------------------------------
+;; Public use
+;;---------------------------------------------------------------------------  
+(defun clog/msg(msg)
+  "Writes MSG with call information to the `clog/-output-buffer'."
+  (clog/-write (format "%s %s"
+		      (clog/-color-text "clog/-MSG:" 'font-lock-constant-face)
+		      (clog/-make-string msg))))
+
+(defun clog/bug(msg)
+  "Writes MSG denated as a bug with call information to the `clog/-output-buffer'."
+  (setq msg (propertize msg 'face 'font-lock-warning-face))
+  (clog/-write (format "%s %s"
+		      (clog/-color-text "clog/-BUG:" 'font-lock-warning-face)
+		      (clog/-make-string msg))))
+
+(defun clog/todo(msg)
+  "Writes MSG denated as a todo with call information to the `clog/-output-buffer'."
+  (clog/-write (format "%s %s"
+		      (clog/-color-text "clog/-TODO:" 'font-lock-variable-name-face)
+		      (clog/-make-string msg))))
+
+;;---------------------------------------------------------------------------
+;; Backtracing
+;;---------------------------------------------------------------------------  
+
+(defun clog/-up-to-func()
   "Returns the nearest backtrace frame that is a direct function call."
-  (let ((func-frame) (height 11)) ;; 10 is when the frames move outside this lib, ;; TODO make this less dumb
+  ;; TODO make this less dumb
+  (let ((func-frame) (height 11)) ;; 11 is when the frames move outside this lib...
     (while (progn (setq func-frame (backtrace-frame height))
-		  (not (clog/frame-is-direct-call-p func-frame)))
+		  (not (clog/-frame-is-direct-call-p func-frame)))
       (setq height (1+ height)))
     func-frame))
 
-(defun clog/frame-is-direct-call-p(frame)
+(defun clog/-frame-is-direct-call-p(frame)
   "Returns true is a backtrace frame is top level function call."
   (if (> (length frame) 1)
       (and (equal (first frame) t)
 	   (fboundp (second frame)))))
 
-(defun clog/find-func-definition(frame)
+(defun clog/-find-func-definition(frame)
   "Returns the name of the file containing the definition of the function that is being
 called in the backtrace frame, FRAME."
-  (symbol-file (clog/get-frame-func frame)))
+  (if (and (fboundp (clog/-get-frame-func frame))
+		    (symbol-file (clog/-get-frame-func frame)))
+      (symbol-file (clog/-get-frame-func frame))
+    (format "Not found, current buffer: %s" (buffer-name))))
 
-
-(defun clog/get-frame-func(frame)
+(defun clog/-get-frame-func(frame)
   "Returns the symbol name of the function being called in FRAME"
   (if (> (length frame) 1)
       (second frame)
     nil))
 
-(defun clog/make-string(msg)
-  "Returns a formated string with a given message, MSG to be logged."
-  (let ((frame (clog/up-to-func)))
-    (format "\"%s\"\n\t%s (%s)\n\t%s %s"
-    	    msg
-	    (clog/-color-text "func:" 'font-lock-keyword-face)
-	    (clog/-color-text (symbol-name (clog/get-frame-func frame))
-			     'font-lock-function-name-face)
-	    (clog/-color-text "file:" 'font-lock-keyword-face)
-	    (clog/-color-text (clog/find-func-definition frame) 'font-lock-string-face))))
-			     
+(defun clog/-frame-func-name(val)
+  "Returns the `symbol-name' of VAL if it is a function else an error message."
+  (if (fboundp val) (symbol-name val) "*none*"))
+
+;;---------------------------------------------------------------------------
+;; Writing and Coloring
+;;---------------------------------------------------------------------------  
+
 (defun clog/-color-text(msg face)
+  "Return the string MSG with the text properties of FACE."
   (propertize msg 'face face))
 
-(defun clog/write(msg &optional buffer)
-  (if (not buffer) (setq buffer (get-buffer "*Messages*")))
-  (if buffer
-      (with-current-buffer buffer
-	(goto-char (point-max))
-	(insert (format "\n%s\n" msg)))
-    (princ "tracker buffer error")))
+(defun clog/-make-string(msg)
+  "Returns a formated string with a given message, MSG to be logged."
+  (let ((frame (clog/-up-to-func)))
+    (format "\"%s\"\n\t%s (%s)\n\t%s %s\n\t%s %s"
+    	    msg
+	    (clog/-color-text "func:" 'font-lock-keyword-face)
+	    (clog/-color-text (clog/-frame-func-name (clog/-get-frame-func frame))
+			     'font-lock-function-name-face)
+	    (clog/-color-text "file:" 'font-lock-keyword-face)
+	    (clog/-color-text (clog/-find-func-definition frame) 'font-lock-string-face)
+	    (clog/-color-text "time:" 'font-lock-keyword-face)
+	    (clog/-color-text (clog/-time-string) 'default))))
 
+(defun clog/-write(msg)
+  "Writes the string MSG to the end of the `clog/-output-buffer'."
+  (with-current-buffer (clog/-get-output-buffer)
+    (goto-char (point-max))
+    (insert (format "\n%s\n" msg))))
 
-
+;;---------------------------------------------------------------------------
+;; Provide it
+;;---------------------------------------------------------------------------  
 (provide 'call-log)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; trace-it.el ends here
+;;; call-log.el
