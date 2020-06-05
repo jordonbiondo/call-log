ae99415b2019436d6c0002b7c8069869ac446314
jordonbiondo <jordonbiondo@gmail.com>
2020-06-05 15:25:40 -0400

    encoding

--- call-log.el
+++ call-log.el
@@ -1,5 +1,5 @@
 ;;; Call-log.el --- Log a message and where in code it came from
-;; 
+;;
 ;; Filename: call-log.el
 ;; Description: Log messages with information about where in the source the message came from.
 ;; Author: Jordon Biondo
@@ -9,46 +9,46 @@
 ;; Last-Updated: Thu Feb  6 12:39:38 2014 (-0500)
 ;;           By: jordon.biondo
 ;;     Update #: 6
-;; URL: 
-;; Doc URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 ;;---------------------------------------------------------------------------
 ;; Output buffer
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 (defvar clog/output-buffer "*Messages*"
   "The name of the buffer where all clog messages will be logged.")
 
@@ -57,12 +57,12 @@
   (let ((buf (get-buffer clog/output-buffer)))
     (if (and buf (bufferp buf)) buf
       (if clog/output-buffer
-	  (generate-new-buffer (generate-new-buffer-name clog/output-buffer))
-	(get-buffer "*Messages*")))))
+          (generate-new-buffer (generate-new-buffer-name clog/output-buffer))
+        (get-buffer "*Messages*")))))
 
 ;;---------------------------------------------------------------------------
 ;; Time stamp
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 (defvar clog/time-format "%Y-%m-%dT%T%z"
   "Format of the time stamp on each log message. See `format-time-string' for details.")
 
@@ -74,29 +74,29 @@ If `clog/time-format is nil, return the default ISO 8601 format"
 
 ;;---------------------------------------------------------------------------
 ;; Emacs lisp use
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 ;;;###autoload
 (defun clog/msg(msg &rest args)
   "Writes MSG with call information to the `clog/-output-buffer'."
   (clog/-write (format "%s %s"
-		      (clog/-color-text "clog/MSG:" 'font-lock-constant-face)
-		      (clog/-make-string (apply 'format (cons msg args))))))
+                       (clog/-color-text "clog/MSG:" 'font-lock-constant-face)
+                       (clog/-make-string (apply 'format (cons msg args))))))
 ;;;###autoload
 (defun clog/bug(msg &rest args)
   "Writes MSG denated as a bug with call information to the `clog/-output-buffer'."
   (setq msg (propertize msg 'face 'font-lock-warning-face))
   (clog/-write (format "%s %s"
-		      (clog/-color-text "clog/BUG:" 'font-lock-warning-face)
-		      (clog/-make-string (apply 'format (cons msg args))))))
+                       (clog/-color-text "clog/BUG:" 'font-lock-warning-face)
+                       (clog/-make-string (apply 'format (cons msg args))))))
 ;;;###autoload
 (defun clog/todo(msg &rest args)
   "Writes MSG denated as a todo with call information to the `clog/-output-buffer'."
   (clog/-write (format "%s %s"
-		      (clog/-color-text "clog/TODO:" 'font-lock-variable-name-face)
-		      (clog/-make-string (apply 'format (cons msg args))))))
+                       (clog/-color-text "clog/TODO:" 'font-lock-variable-name-face)
+                       (clog/-make-string (apply 'format (cons msg args))))))
 ;;---------------------------------------------------------------------------
 ;; Interactive uses
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 (defmacro clog/-make-stamper(name)
   "Creates clog/the insert-stamped-x functions, internal use only."
   `(progn
@@ -105,7 +105,7 @@ If `clog/time-format is nil, return the default ISO 8601 format"
        (interactive)
        (insert ,(format "(clog/%s \"" name))
        (save-excursion
-	 (insert (format " : %s @ %s\")" (or (getenv "USERNAME") (getenv "USER")) (clog/-time-string)))))))
+         (insert (format " : %s @ %s\")" (or (getenv "USERNAME") (getenv "USER")) (clog/-time-string)))))))
 
 (clog/-make-stamper "msg")
 (clog/-make-stamper "bug")
@@ -113,14 +113,14 @@ If `clog/time-format is nil, return the default ISO 8601 format"
 
 ;;---------------------------------------------------------------------------
 ;; Backtracing
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 
 (defun clog/-up-to-func()
   "Returns the nearest backtrace frame that is a direct function call."
   ;; TODO make this less dumb
   (let ((func-frame) (height 11)) ;; 11 is when the frames move outside this lib...
     (while (progn (setq func-frame (backtrace-frame height))
-		  (not (clog/-frame-is-direct-call-p func-frame)))
+                  (not (clog/-frame-is-direct-call-p func-frame)))
       (setq height (1+ height)))
     func-frame))
 
@@ -128,13 +128,13 @@ If `clog/time-format is nil, return the default ISO 8601 format"
   "Returns true is a backtrace frame is top level function call."
   (if (> (length frame) 1)
       (and (equal (first frame) t)
-	   (fboundp (second frame)))))
+           (fboundp (second frame)))))
 
 (defun clog/-find-func-definition(frame)
   "Returns the name of the file containing the definition of the function that is being
 called in the backtrace frame, FRAME."
   (if (and (fboundp (clog/-get-frame-func frame))
-		    (symbol-file (clog/-get-frame-func frame)))
+           (symbol-file (clog/-get-frame-func frame)))
       (symbol-file (clog/-get-frame-func frame))
     (format "Not found, current buffer: %s" (buffer-name))))
 
@@ -150,7 +150,7 @@ called in the backtrace frame, FRAME."
 
 ;;---------------------------------------------------------------------------
 ;; Writing and Coloring
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 
 (defun clog/-color-text(msg face)
   "Return the string MSG with the text properties of FACE."
@@ -160,14 +160,14 @@ called in the backtrace frame, FRAME."
   "Returns a formated string with a given message, MSG to be logged."
   (let ((frame (clog/-up-to-func)))
     (format "\"%s\"\n\t%s (%s)\n\t%s %s\n\t%s %s"
-    	    msg
-	    (clog/-color-text "func:" 'font-lock-keyword-face)
-	    (clog/-color-text (clog/-frame-func-name (clog/-get-frame-func frame))
-			     'font-lock-function-name-face)
-	    (clog/-color-text "file:" 'font-lock-keyword-face)
-	    (clog/-color-text (clog/-find-func-definition frame) 'font-lock-string-face)
-	    (clog/-color-text "time:" 'font-lock-keyword-face)
-	    (clog/-color-text (clog/-time-string) 'default))))
+            msg
+            (clog/-color-text "func:" 'font-lock-keyword-face)
+            (clog/-color-text (clog/-frame-func-name (clog/-get-frame-func frame))
+                              'font-lock-function-name-face)
+            (clog/-color-text "file:" 'font-lock-keyword-face)
+            (clog/-color-text (clog/-find-func-definition frame) 'font-lock-string-face)
+            (clog/-color-text "time:" 'font-lock-keyword-face)
+            (clog/-color-text (clog/-time-string) 'default))))
 
 (defun clog/-write(msg)
   "Writes the string MSG to the end of the `clog/-output-buffer'."
@@ -177,7 +177,7 @@ called in the backtrace frame, FRAME."
 
 ;;---------------------------------------------------------------------------
 ;; Provide it
-;;---------------------------------------------------------------------------  
+;;---------------------------------------------------------------------------
 (provide 'call-log)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; call-log.el
